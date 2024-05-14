da0f4d37a595de9225927c506365f64b4db8f024
konas122 <2407626470@qq.com>
2024-05-14 20:48:43 +0800

    add `Array`

--- vm/vm.go
+++ vm/vm.go
@@ -139,6 +139,16 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpArray:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			array := vm.buildArray(vm.sp-numElements, vm.sp)
+			vm.sp = vm.sp - numElements
+
+			if err := vm.push(array); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -275,4 +285,12 @@ func (vm *VM) executeMinusOperator() error {
 
 	value := operand.(*object.Integer).Value
 	return vm.push(&object.Integer{Value: -value})
+}
+
+func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
+	elements := make([]object.Object, endIndex-startIndex)
+	for i := startIndex; i < endIndex; i++ {
+		elements[i-startIndex] = vm.stack[i]
+	}
+	return &object.Array{Elements: elements}
 }
